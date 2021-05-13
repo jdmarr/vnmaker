c234a6267a1e3a47e2d17c8e7a218a2c97c9b064
Jordan Marr <jmarr.to@gmail.com>
2021-05-13 10:12:40 -0700

    Remove example sql code

--- model/appModel.js
+++ model/appModel.js
@@ -116,44 +116,6 @@ User.findOrCreateByGitHubId = function(user, result) {
     }
   });
 };
-// Task.getAllTask = function (result) {
-//         sql.query("Select * from tasks", function (err, res) {
-//
-//                 if(err) {
-//                     console.log("error: ", err);
-//                     result(null, err);
-//                 }
-//                 else{
-//                   console.log('tasks : ', res);
-//
-//                  result(null, res);
-//                 }
-//             });
-// };
-// Task.updateById = function(id, task, result){
-//   sql.query("UPDATE tasks SET task = ? WHERE id = ?", [task.task, id], function (err, res) {
-//           if(err) {
-//               console.log("error: ", err);
-//                 result(null, err);
-//              }
-//            else{
-//              result(null, res);
-//                 }
-//             });
-// };
-// Task.remove = function(id, result){
-//      sql.query("DELETE FROM tasks WHERE id = ?", [id], function (err, res) {
-//
-//                 if(err) {
-//                     console.log("error: ", err);
-//                     result(null, err);
-//                 }
-//                 else{
-//
-//                  result(null, res);
-//                 }
-//             });
-// };
 
 //Image object constructor
 var Image = function(image) {
