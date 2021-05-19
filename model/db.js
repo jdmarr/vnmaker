9b85742e8ea009c948fa5ed22ab73c101949193f
Jordan Marr <jmarr.to@gmail.com>
2021-05-18 20:48:51 -0700

    Use connection pool

--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2');
 require("dotenv").config();
 
 //local mysql db connection
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     host     : process.env.MYSQL_HOST,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
