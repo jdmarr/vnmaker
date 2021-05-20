b29c1e1c94351584eb8b1c080e66809dacfa2769
Jordan Marr <jmarr.to@gmail.com>
2021-05-20 09:51:30 -0700

    Use env vars for mysql host and database

--- model/db.js
+++ model/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : process.env.MYSQL_HOST,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-    database : 'vndb'
+    database : process.env.MYSQL_DATABASE
 });
 
 connection.connect(function(err) {
